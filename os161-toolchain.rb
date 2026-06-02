30dae696ddfffdf6fd6b02afcc1a9e7c97fa36de
Ivan Velickovic <i.velickovic@unsw.edu.au>
2026-06-02 12:54:18 +1000

    os161-toolchain.rb fix

--- os161-toolchain.rb
+++ os161-toolchain.rb
@@ -1,4 +1,7 @@
 class Os161Toolchain < Formula
+    homepage "https://github.com/Ivan-Velickovic/os161-homebrew"
+    url "https://github.com/Ivan-Velickovic/os161-homebrew"
+
     depends_on "os161-binutils"
     depends_on "os161-gcc"
     depends_on "os161-gdb"
